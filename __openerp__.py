7cd7858edcb02dd3088f8324aa08da2c4ad0a603
Doaa <doaanegm1@gmail.com>
2016-06-01 23:42:44 +0200

    my work

---
+++ __openerp__.py
@@ -0,0 +1,12 @@
+{
+    'name':'ITI HR',
+    'version': '1.0',
+    'depends':['hr','hr_attendance'],
+    'description': """
+ITI HR
+==========================
+    """,
+    'data': [
+        'odoo_hr_view.xml'
+    ],
+}
